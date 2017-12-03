c685410ca7d6d78db740ab928c1d932e326fc7de
kszczurowski <kszczurowski@absolute.com>
2017-12-03 00:11:56 -0800

    Added application's mechanics; Refactor SharedObjects and Sevice projects; Added dummy date to the PatientsService project;

---
+++ PatientViewer.SharedObjects/Patient.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatientViewer.SharedObjects
+{
+    public class Patient
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string MobileNumber { get; set; }
+        public string Address { get; set; }
+    }
+}
--- PatientWpfApp/Patient.xaml.cs
+++ PatientWpfApp/Patient.xaml.cs
@@ -19,7 +19,9 @@ using MahApps.Metro.Controls;
 namespace PatientWpfApp
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Patient.xaml
+    /// Krzysztof Szczurowski
+    /// Repo location: https://github.com/kriss3/BCIT_WPF_COMP3608_Final_PatientApp.git
     /// </summary>
     public partial class Patient : MetroWindow
     {
